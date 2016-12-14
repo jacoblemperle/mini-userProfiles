5f9c7be5ce07ebafefb87bd6fcb0d7d573720cb5
jacoblemperle <jacob.lemperle@gmail.com>
2016-12-14 11:57:33 -0700

    first commit

--- controller.js
+++ controller.js
@@ -1,5 +1,10 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
-})
+angular.module('userProfiles').controller('MainController', function($scope, svc){
+
+    $scope.getUsers = function(){
+      $scope.users = svc.getUsers();
+    };
+
+    $scope.getUsers();
+    $scope.toggleFavorite = svc.toggleFavorite;
+
+});
